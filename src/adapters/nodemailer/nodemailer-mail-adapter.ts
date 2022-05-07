ea28cc25a030b5c89966b658c17a03c694362881
geovane-gc <geovanegallotticazal@hotmail.com>
2022-05-07 12:36:13 -0300

    server

---
+++ src/adapters/nodemailer/nodemailer-mail-adapter.ts
@@ -0,0 +1,22 @@
+import { MailAdapter, SendMailData } from "../mail-adapter";
+import nodemailer from "nodemailer";
+
+const transport = nodemailer.createTransport({
+  host: "smtp.mailtrap.io",
+  port: 2525,
+  auth: {
+    user: "27af673a180efa",
+    pass: "c21475f7868167",
+  },
+});
+
+export class NodemailerMailAdapter implements MailAdapter {
+  async sendMail({ subject, body }: SendMailData) {
+    await transport.sendMail({
+      from: "Equipe Feedget <oi@feedget.com>",
+      to: "Geovane Gallotti <geovanegc123@gmail.com>",
+      subject,
+      html: body,
+    });
+  }
+}
